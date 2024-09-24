dd8ab1bbc2386f77334f5192ef9dfbbda5f86102
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-09-24 06:37:33 +0530

    Add Assets, Directories and Pages, create Sidebar

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PersonalRoom() {
+    return (
+        <div>
+            PersonalRoom
+        </div>
+    )
+}
+
+export default PersonalRoom
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recordings() {
+  return (
+    <div>
+      Recordings
+    </div>
+  )
+}
+
+export default Recordings
